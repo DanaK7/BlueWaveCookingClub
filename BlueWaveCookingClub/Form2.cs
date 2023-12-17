885caee45ad6dd6d103ca209d627e01fd4d56e96
Dana Jade Selena <danak200020@gmail.com>
2023-12-17 14:03:33 +0530

    Login Code updates

--- BlueWaveCookingClub/Form2.cs
+++ BlueWaveCookingClub/Form2.cs
@@ -12,6 +12,12 @@ namespace BlueWaveCookingClub
 {
     public partial class FrmLogin : Form
     {
+
+        //Admin and member passwords
+        private const string AdminUsername = "admin";
+        private const string AdminPassword = "admin123";
+        private const string MemberUsername = "member";
+        private const string MemberPassword = "member123";
         public FrmLogin()
         {
             InitializeComponent();
@@ -19,7 +25,47 @@ namespace BlueWaveCookingClub
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-          
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (username.ToLower() == AdminUsername && password == AdminPassword)
+            {
+                MessageBox.Show("Admin login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                OpenAdminPanel();
+            }
+            else if (username.ToLower() == MemberUsername && password == MemberPassword)
+            {
+                MessageBox.Show("Member login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                OpenRegistrationForm();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clear the input fields
+            txtUsername.Clear();
+            txtPassword.Clear();
+        }
+
+        private void rbtnShow_CheckedChanged(object sender, EventArgs e)
+        {
+            // Toggle password visibility
+            txtPassword.PasswordChar = chkShowPassword.Checked ? '\0' : '*';
+        }
+
+        private void linklblReg_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Click event for the registration link label
+            OpenRegistrationForm();
         }
     }
 }
