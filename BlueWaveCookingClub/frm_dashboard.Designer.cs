13eda8ce8d856bb1eb41e35052cff4f9f05128d0
Dana Jade Selena <danak200020@gmail.com>
2024-01-03 19:04:08 +0530

    Updates

--- BlueWaveCookingClub/frm_dashboard.Designer.cs
+++ BlueWaveCookingClub/frm_dashboard.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDashboard));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblDashboard = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
@@ -41,22 +42,34 @@
             this.panel1.Size = new System.Drawing.Size(322, 458);
             this.panel1.TabIndex = 0;
             // 
+            // lblDashboard
+            // 
+            this.lblDashboard.AutoSize = true;
+            this.lblDashboard.Location = new System.Drawing.Point(469, 19);
+            this.lblDashboard.Name = "lblDashboard";
+            this.lblDashboard.Size = new System.Drawing.Size(75, 13);
+            this.lblDashboard.TabIndex = 1;
+            this.lblDashboard.Text = "DASHBOARD";
+            // 
             // FrmDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Tan;
             this.ClientSize = new System.Drawing.Size(720, 454);
+            this.Controls.Add(this.lblDashboard);
             this.Controls.Add(this.panel1);
             this.Name = "FrmDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dashboard";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblDashboard;
     }
 }
